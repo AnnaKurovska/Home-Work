97505e909a3ee52352f37832316b2aedae9291dd
AnnaKurovska <e_kontakt@mail.ru>
2016-06-09 15:01:31 +0300

    Add files via upload

---
+++ 20lesson/script.js
@@ -0,0 +1,64 @@
+/**
+ * Created by Anna on 06.06.2016.
+ */
+
+/*Первое задание*/
+var month = prompt('Какой сейчас месяц?');
+
+if ( month == 'Cентябрь') {
+    alert( 'Вы правы!' );
+} else {
+    alert( 'Та ну ты что? Сентябрь!' );
+}
+
+/*Второе задание*/
+var x = prompt('Введите любое число');
+
+if (x > 0) {
+    alert (1);
+} else if (x < 0) {
+    alert (-1);
+} else if (x == 0) {
+    alert (0);
+}
+
+
+/*Третье задание*/
+var x = 4, y = 6;
+alert ( (x + y >= 9) ? 'Да!' : 'Нет!');
+
+
+/*Четверное задание*/
+
+var z = prompt('Введите любое число больше 100');
+
+    while (z < 100) {
+       alert ( z = prompt('Введите любое число больше 100') );
+    } if (z >= 100) {
+        alert (z);
+        brake
+    }
+
+
+
+/*Пятое задание*/
+var a = +prompt('a?', '');
+switch (a) {
+    case 0:
+        alert ( 0 )
+        brake
+    case 1:
+        alert ( 1 )
+        brake
+    case 2:
+    case 3:
+        alert("2,3")
+        brake
+}
+
+
+
+
+
+
+
